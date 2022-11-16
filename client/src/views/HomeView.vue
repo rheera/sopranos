c01af617c4c74203934fc8bae9c99e8e389a967c
rheera <rheera2@uwo.ca>
2022-11-16 17:26:55 -0500

    Initial commit

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { quotes } from "@/assets/data/quotes.js";
+import { ref, computed } from "vue";
+
+let quoteArr = ref(quotes);
+let quoteNum = ref(Math.floor(Math.random() * quoteArr.value.length));
+
+function randomize() {
+  let oldNum = quoteNum.value;
+  quoteNum.value = Math.floor(Math.random() * quoteArr.value.length);
+  quoteNum.value == oldNum ? randomize() : null;
+}
+
+const countVotes = computed(() => {
+  return quoteArr.value[quoteNum.value].upvote >=
+    quoteArr.value[quoteNum.value].downvote
+    ? quoteArr.value[quoteNum.value].upvote -
+        quoteArr.value[quoteNum.value].downvote
+    : 0;
+});
+</script>
+
+<template>
+  <main>
+    <div class="app-wrapper container">
+      <div class="quote-cnt">
+        <h3 class="quote">{{ quoteArr[quoteNum].quote }}</h3>
+        <p class="author">- {{ quoteArr[quoteNum].author }}</p>
+      </div>
+      <div class="vote-btns">
+        <span class="vote vote-up" @click="quoteArr[quoteNum].upvote++">
+          <svg width="36" height="36">
+            <path d="M2 10h32L18 26 2 10z" fill="currentColor"></path>
+          </svg>
+        </span>
+        <p class="votes">{{ countVotes }}</p>
+        <span class="vote vote-down" @click="quoteArr[quoteNum].downvote++">
+          <svg width="36" height="36">
+            <path d="M2 10h32L18 26 2 10z" fill="currentColor"></path>
+          </svg>
+        </span>
+      </div>
+      <button class="btn btn-primary" @click="randomize()">Random Quote</button>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+.app-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  // max-width: 900px;
+  // margin: 0 auto;
+  .quote-cnt {
+  }
+  .vote-btns {
+    display: flex;
+    .vote {
+      display: inline-block;
+      color: #687074;
+      &:hover {
+        cursor: pointer;
+      }
+      &-up {
+        transform: rotate(180deg) translateY(5px);
+      }
+    }
+  }
+}
+</style>
---
+++ client/vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/scss/_variables.scss";
+        @import "@/scss/_mixins.scss";
+        @import "@/scss/_themes.scss";
+        @import "@/scss/_fonts.scss";
+        @import "@/scss/_global.scss";
+        @import "@/scss/_helpers.scss";
+        @import "@/scss/_vendor.scss";
+        @import "@/scss/_reset.scss";
+      `,
+      },
+    },
+  },
+});
